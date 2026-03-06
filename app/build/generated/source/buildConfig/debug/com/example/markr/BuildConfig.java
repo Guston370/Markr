350b8a6fd4c8d5dbb511b690d531e4050ce9003a
Aditya Joshi <guston370@gmail.com>
2026-03-06 18:07:32 +0530

    Update

---
+++ app/build/generated/source/buildConfig/debug/com/example/markr/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.markr;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.markr";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "2.3.0";
+  // Field from default config.
+  public static final String SUPABASE_ANON_KEY = "sb_publishable_8KdRkxumAWq77bdX9Xpdgg_Tan8nrZa";
+  // Field from default config.
+  public static final String SUPABASE_URL = "https://wvoelfjpbxqtfvaxaylc.supabase.co";
+}
